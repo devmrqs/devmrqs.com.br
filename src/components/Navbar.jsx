1906a701e08c176b0e764957ddbae67d9db79301
over <overboboto@gmail.com>
2026-08-30 16:51:23 -0300

    🎨 style: ajuste os parâmetros de escala da Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   const linkClasses = ({ isActive }) =>
     isActive
-      ? "bg-[rgba(128,139,114,0.2)] px-5 py-2 -my-2 rounded-full duration-500 text-[rgba(0,0,0,0.8)]"
-      : "px-3 py-2 -my-2 rounded-full hover:text-[rgba(0,0,0,0.8)] duration-500";
+      ? "bg-[rgb(135,150,118,0.25)] px-4 py-1.5 -my-1.5 rounded-full duration-500 text-[rgba(0,0,0,0.8)]"
+      : "px-2.5 py-1.5 -my-1.5 rounded-full hover:text-[rgba(0,0,0,0.8)] duration-500";
 
   return (
-    <div className="flex flex-row mt-24 justify-between items-center gap-3 select-none">
-      <nav className="bg-white flex justify-between items-center gap-6 rounded-full p-3 w-3xl border-[0.5px] border-[rgba(0,0,0,0.3)] text-[rgba(0,0,0,0.6)]">
-        <div className="flex gap-6 font-semibold ml-0.5 text-base">
+    <div className="flex flex-row mt-24 justify-between items-center gap-2 select-none">
+      <nav className="bg-white flex justify-between items-center gap-4 rounded-full p-2 w-2xl border-[0.5px] border-[rgba(0,0,0,0.1)] text-[rgba(0,0,0,0.6)]">
+        <div className="flex gap-4 font-semibold ml-0.5 text-sm">
           <NavLink to="/" className={linkClasses}>
             Início
           </NavLink>
@@ -20,16 +20,22 @@ const Navbar = () => {
             Portfólio
           </NavLink>
         </div>
-        <i className="bi bi-translate mr-5 text-xl hover:text-[rgba(0,0,0,0.8)] hover:scale-110 duration-500"></i>
+        <i className="bi bi-translate mr-3 text-base hover:text-[rgba(0,0,0,0.8)] hover:scale-110 duration-500"></i>
       </nav>
       <a
         href="https://www.linkedin.com/in/devmrqs/"
         target="_blank"
         rel="noopener noreferrer"
-        className="bg-[rgba(255,255,255,0.7)] px-4.5 py-3 border-[0.5px] border-[rgba(0,0,0,0.3)] text-[rgb(0,0,0,0.8)] rounded-full hover:scale-110 hover:bg-[rgba(255,255,255,0.85)] duration-500 font-bold"
+        className="bg-[rgba(255,255,255,0.3)] px-3 py-2 border-[0.5px] border-[rgba(0,0,0,0.08)] text-[rgb(0,0,0,0.8)] text-sm rounded-full hover:scale-110 hover:bg-white duration-500 font-bold"
       >
         In
       </a>
+      <a
+        className="bi bi-github bg-[rgba(255,255,255,0.3)] px-3 py-2 border-[0.5px] border-[rgba(0,0,0,0.08)] text-[rgb(0,0,0,0.8)] text-sm rounded-full hover:scale-110 hover:bg-white duration-500 font-bold"
+        href="https://github.com/devmrqs"
+        target="_blank"
+        rel="noopener noreferrer"
+      ></a>
     </div>
   );
 };
