855f68b1e11453fcdc13b2acfc9a66cef0c728e1
over <overboboto@gmail.com>
2026-08-30 23:41:51 -0300

    ♻️ refactor: adiciona variante hover para shadowBox

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import perfil from "../assets/perfil.png";
+import { shadowBox } from "../styles/sharedClasses";
 
 const Header = () => {
   return (
@@ -6,7 +7,7 @@ const Header = () => {
       <img
         src={perfil}
         alt=""
-        className="rounded-full select-none h-auto w-28"
+        className={`rounded-full select-none h-auto w-28 ${shadowBox}`}
       />
       <div className="flex flex-col ml-6 justify-center">
         <h3 className="font-bold text-lg mb-1.5 select-text">
@@ -19,7 +20,7 @@ const Header = () => {
           <i className="bi bi-geo mr-2 text-base text-[rgba(0,0,0,0.7)]"></i>
           <p className="text-[rgba(0,0,0,0.7)] text-base">Brasil - RJ</p>
           <i className="bi bi-circle-fill text-[3px] text-[rgba(0,0,0,0.7)] ml-5 mr-5"></i>
-          <div className="bg-[rgba(116,187,113,0.2)] hover:bg-[rgba(127,187,125,0.35)] flex items-center justify-center gap-2 py-1.5 px-4 rounded-full border-[0.5px] border-[rgb(44,85,62,0.2)] duration-500">
+          <div className="bg-[rgba(116,187,113,0.2)] flex items-center justify-center gap-2 py-1.5 px-4 rounded-full border-[0.5px] border-[rgb(44,85,62,0.2)] duration-500">
             <span className="relative flex h-2.5 w-2.5">
               <span className="animate-ping absolute h-full w-full rounded-full bg-[rgb(62,121,87)] opacity-75"></span>
               <span className="relative rounded-full h-2.5 w-2.5 bg-[rgb(62,121,87)]"></span>
--- src/styles/sharedClasses.js
+++ src/styles/sharedClasses.js
@@ -1,7 +1,10 @@
 export const shadowBox =
-  "shadow-[0px_2px_4px_rgba(0,0,0,0.3),0px_7px_13px_-3px_rgba(0,0,0,0.1),0px_-2px_0px_inset_rgba(0,0,0,0.1)]";
+  "shadow-[0px_2px_4px_rgba(0,0,0,0.2),0px_7px_13px_-3px_rgba(0,0,0,0.1),0px_-1px_0px_inset_rgba(0,0,0,0.1)]";
 
-export const roundButtonClasses = `bg-[rgba(255,255,255,0.3)] px-4 py-2.5 border-[0.5px] border-[rgba(0,0,0,0.08)] text-[rgb(0,0,0,0.8)] text-base rounded-full hover:scale-110 hover:bg-white duration-500 font-bold ${shadowBox}`;
+export const hoverShadowBox =
+  "hover:shadow-[0px_2px_4px_rgba(0,0,0,0.2),0px_7px_13px_-3px_rgba(0,0,0,0.1),0px_-1px_0px_inset_rgba(0,0,0,0.1)]";
+
+export const roundButtonClasses = `bg-[rgba(255,255,255,0.3)] px-4 py-2.5 border-[0.5px] border-[rgba(0,0,0,0.08)] text-[rgb(0,0,0,0.8)] text-base rounded-full hover:scale-110 hover:bg-white duration-500 font-bold ${hoverShadowBox}`;
 
 export const themeButtonClasses = `w-full flex items-center gap-2.5 px-4 py-2 text-base hover:bg-[rgba(0,0,0,0.05)] hover:rounded-full duration-300`;
 
